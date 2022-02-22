5b667ef2c92645eb92f05dce0cde1149d791f4be
Cole Rossman <colerossman99@gmail.com>
2022-02-22 12:04:03 -0800

    test for renderWorkshop passes

--- test/render-utils.test.js
+++ test/render-utils.test.js
@@ -1,18 +1,17 @@
-// IMPORT MODULES under test here:
-// import { example } from '../example.js';
+import { renderWorkshop, renderParticipant } from '../render-utils.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('renderWorkshop should render a div with a class of workshop and an h3 with the workshop name', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = `<div class="workshop"><h3>Advanced spearfishing workshop</h3></div>`;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = renderWorkshop({ name: 'Advanced spearfishing workshop' });
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    expect.equal(actual.outerHTML, expected);
 });
