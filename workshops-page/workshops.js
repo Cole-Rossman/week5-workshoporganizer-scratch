18f2525efec6577ea6bb8c69f5df71e17b7e3723
Cole Rossman <colerossman99@gmail.com>
2022-02-22 12:42:24 -0800

    displayWorkshops displays workshop and participant

--- workshops-page/workshops.js
+++ workshops-page/workshops.js
@@ -7,3 +7,22 @@ const logoutButton = document.getElementById('logout');
 logoutButton.addEventListener('click', async () => {
     await logout();
 });
+
+async function displayWorkshops() {
+    // fetch all workshops from supabase
+    const workshops = await fetchWorkshops();
+    // clear out workshopList
+    workshopList.textContent = '';
+    // loop and append for workshops
+    for (let workshop of workshops) {
+        const workshopEl = renderWorkshop(workshop);
+
+        for (let participant of workshop.participants) {
+            const participantEl = renderParticipant(participant);
+
+            workshopEl.append(participantEl);
+        }
+        workshopList.append(workshopEl);
+    }
+}
+displayWorkshops();
