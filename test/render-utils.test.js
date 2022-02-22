ff37da753c6f8507800273cee4e9430a0376efa0
Cole Rossman <colerossman99@gmail.com>
2022-02-22 12:07:27 -0800

    renderParticipant test passes

--- test/render-utils.test.js
+++ test/render-utils.test.js
@@ -15,3 +15,17 @@ test('renderWorkshop should render a div with a class of workshop and an h3 with
     // Make assertions about what is expected versus the actual result
     expect.equal(actual.outerHTML, expected);
 });
+
+test('renderParticipant should render a div with a class of participants and a p element with a class of participant and the name of the participant', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = `<div class="participants"><p class="participant">Johnny Steel</p></div>`;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = renderParticipant({ name: 'Johnny Steel' });
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual.outerHTML, expected);
+});
