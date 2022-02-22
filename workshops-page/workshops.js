0cee75200400b704b2eed3b16535cb2a523f4fcc
Cole Rossman <colerossman99@gmail.com>
2022-02-22 12:23:25 -0800

    logout eventlistener works in workshops.js

--- workshops-page/workshops.js
+++ workshops-page/workshops.js
@@ -1,2 +1,9 @@
 import { checkAuth, fetchWorkshops, logout } from '../fetch-utils.js';
 import { renderWorkshop, renderParticipant } from '../render-utils.js';
+
+const workshopList = document.getElementById('workshop-list');
+const logoutButton = document.getElementById('logout');
+
+logoutButton.addEventListener('click', async () => {
+    await logout();
+});
